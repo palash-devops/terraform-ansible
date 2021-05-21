339e6bd74feba2db1cce8fa01e8784ce39452fb0
Palash Sorte <mr.palashsorte@gmail.com>
2021-05-21 22:04:38 +0530

    committing changes

---
+++ terraform/variable.tf
@@ -0,0 +1,12 @@
+variable "AKIAV6JYNG6FQAHVBXZS" { }
+variable "0QEvwttYjj0T1BceqoSy381zSoqkrz1ddMj8SigI" { }
+variable "region" {
+  default = "us-east-2"
+}
+variable "availability_zone" {
+  default = "us-east-2a"
+}
+variable "environment_tag" {
+  description = "Environment tag"
+  default = "dev"
+}
